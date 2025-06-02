cecfb0e36ca4b768f94e776c105de5bac9960a68
Elena <nicolae.ejemplo@gmail.com>
2025-06-01 20:10:18 -0500

    amuna

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// Inicializar AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000,      // Duración de la animación en ms
+  easing: 'ease-in-out',
+  once: true,          // Si quieres que se animen solo una vez
+  mirror: false        // Si quieres que se repitan al hacer scroll hacia arriba
+});
+
+// Si necesitas más funcionalidades de JS, las puedes agregar aquí
